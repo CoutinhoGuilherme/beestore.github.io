3c6af1e822ba30823ab50e2b3c6327de5483b8da
CoutinhoGuilherme <69174628+CoutinhoGuilherme@users.noreply.github.com>
2020-08-16 15:11:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+  function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+
+ 
